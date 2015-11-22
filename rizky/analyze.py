0ffed4b2d111818c06222a54bb95bed74f86f70f
risoll <rzkysol@gmail.com>
2015-11-22 14:06:30 +0700

    use jester-100 file

--- rizky/analyze.py
+++ rizky/analyze.py
@@ -1,67 +1,57 @@
+__author__ = 'Rizky Solechudin'
+
 import xlrd #import library pembaca excel
 import math
 import os
+filename = "../jester-data-100.xls"
 
-# def getData():
-# 	book = xlrd.open_workbook("jester-data-1.xls") #open file jester
-# 	sh = book.sheet_by_index(0) #masukkan isi file jester matrix sh
-# 	return sh
+def checkFile(filename):
+	if os.path.isfile(filename):
+		print "File: ",filename
+	else:
+		print "Failed to open file"
 
-Data = [[0 for x in range(6)] for x in range(6)]
-Data[0][0] = 2
-Data[0][3] = 4
-Data[0][4] = 5
-Data[1][0] = 5
-Data[1][2] = 4
-Data[1][5] = 1
-Data[2][2] = 5
-Data[2][4] = 2
-Data[3][1] = 1
-Data[3][3] = 5
-Data[3][5] = 4
-Data[4][2] = 4
-Data[4][5] = 2
-Data[5][0] = 4
-Data[5][1] = 5
-Data[5][3] = 1
+def getData(filename):
+	book = xlrd.open_workbook(filename) #open file jester
+	sh = book.sheet_by_index(0) #masukkan isi file jester matrix sh
+	return sh
 
-if os.path.isfile("jester-data-1.xls"):
-	print "ganteng"
-else:
-	print "fail"
+dataTemp = getData(filename)
+COLS_COUNT = dataTemp.ncols
+ROWS_COUNT = dataTemp.nrows
+COLS_COUNT = COLS_COUNT
+ROWS_COUNT = ROWS_COUNT - 2 #perbaikan perhitungan total baris pada file jester
+
+def getRatingTemp (user,item): 
+	return dataTemp.row(user)[item].value #return value dari matrix sh
+
+data = [[0 for x in range(COLS_COUNT)] for x in range(ROWS_COUNT)]
+
+for x in range(ROWS_COUNT): #masukkan data jester kedalam array
+	for y in range(COLS_COUNT):
+		data[x][y] = getRatingTemp(x,y)
 
 def getRating (user,item): 
-	return Data[user][item]
-
-
-# def getRating (user,item): 
-# 	return data.row(user)[item].value #return value dari matrix sh
-
-# data = getData()
-
-print getRating(4,0)
+	return data[user][item]
 
 def getItem(n): #get item yang pernah dirating user n
 	A = []
-	for i in range (0,6):
-		if(getRating(n,i) != 0):
+	for i in range (1,COLS_COUNT):
+		if(getRating(n,i) != 99):
 			A.append(i)
 	return A
 
-print getItem(1)
-print len(getItem(1))
-
-def getItemBersama(u,v):
+def getItemBersama(u,v): #get item yang sama-sama pernah dirating user u dan user v
 	A = []
-	for i in range(0,6):
-		if(getRating(u,i) != 0 and getRating(v,i) != 0):
+	for i in range(1,COLS_COUNT):
+		if(getRating(u,i) != 99 and getRating(v,i) != 99):
 			A.append(i)
 	return A
 
 def getHole(n):
 	A = []
-	for i in range(0,6):
-		if(getRating(n,i) == 0):
+	for i in range(1,COLS_COUNT):
+		if(getRating(n,i) == 99):
 			A.append(i)
 	return A
 
@@ -69,11 +59,11 @@ def getNeighbors(n): #get neighbors user n berdasarkan kesamaan item dari fungsi
 	A = getItem(n)
 	B = []
 	found = 0
-	for i in range(0,6):
+	for i in range(ROWS_COUNT):
 		found = 0
 		if(i != n):
 			for j in A:
-				if(getRating(i,j) != 0): #jika kesamaan item sudah != 0, maka user i menjadi neighbor user n
+				if(getRating(i,j) != 99): #jika kesamaan item sudah != 0, maka user i menjadi neighbor user n
 					found = 1
 					break
 			if(found == 1):
@@ -97,7 +87,7 @@ def getSim(n,o): #get similaritas antara user n dan user o
 	rAvg1 = getAverageRating(n)
 	rAvg2 = getAverageRating(o)
 	y = getItemBersama(n,o)
-	for i in y: #rumus gan
+	for i in y:
 		r1 = getRating(n,i)
 		r1 = r1 - rAvg1
 		r2 = getRating(o,i)
@@ -109,7 +99,7 @@ def getSim(n,o): #get similaritas antara user n dan user o
 		rBot1 = rBot1 + r4
 		rBot2 = rBot2 + r5
 	rBot = math.sqrt(rBot1 * rBot2)
-	return rTop/rBot #rumus gan
+	return rTop/rBot 
 
 def sortSims(sims): #fungsi quicksort untuk mendapatkan top similarity
 	if(len(sims) > 1):
@@ -127,7 +117,7 @@ def sortSims(sims): #fungsi quicksort untuk mendapatkan top similarity
 		sims[:] = smallerItems + [sims[pivotIndex]] + largerItems
 	return sims
 
-def getPredictedRating(n,i):
+def getPredictedRating(n,i): #get prediksi rating antara user n dengan item i
 	avgN = getAverageRating(n)
 	neighbors = getNeighbors(n)
 	top = 0.0
@@ -136,28 +126,21 @@ def getPredictedRating(n,i):
 	totalBot = 0.0
 	for m in neighbors:
 		sim = getSim(n,m)
-		bot = sim
-		if(getRating(m,i) != 0 ):
+		if(getRating(m,i) != 99 ):
 			top = sim * (getRating(m,i) - getAverageRating(m))
 			totalTop = totalTop + top
+		bot = sim
 		if(sim < 0):
-			bot = sim * -1
+			bot = bot * -1
 		totalBot = totalBot + bot
 	return avgN + totalTop/totalBot 
 
-n = 4
-holeItems = getHole(n)
-for i in holeItems:
-	print "User " ,n,", Item ", i, " : ", getPredictedRating(n,i)
-# print Data
-# print getNeighbors(4)
-# print getPredictedRating(4,1)
-#print "Target: User 2"
-
-#m = sortSims(sims)
-#n = len(m)
-#topSims = []
-#for i in range(0,20):
-#	topSims.append(m[n-1-i])
-#print len(topSims)
-#print topSims
+def main():
+	print checkFile(filename)
+	print "Total kolom: ",COLS_COUNT
+	print "Total baris: ",ROWS_COUNT
+	for i in range(ROWS_COUNT): 
+		for j in range(1,COLS_COUNT):
+	 		print "User " ,i,", Item ", j, " : ", getPredictedRating(i,j) #get semua predicted rating yang ada
+ 
+main()
